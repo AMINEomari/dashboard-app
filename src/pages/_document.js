ae0d2db417144a3d0e9cf4b7c4976f6fb67d8c0b
amine alaoui <omari1amineco@gmail.com>
2024-05-13 20:55:52 +0100

    fix: app icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <title>dashboard app</title>
+        <link rel="icon" sizes="32x32" href="/Logo.svg" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
